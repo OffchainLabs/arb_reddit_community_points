7d372008c9c809684b49017de81ec8a14c333f44
dzgoldman <dzgoldman@wesleyan.edu>
2020-07-28 09:45:28 -0400

    deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,27 @@ var SubredditPoints_v0 = artifacts.require("SubredditPoints_v0");
 var Subscriptions_v0 = artifacts.require("Subscriptions_v0")
 var Distributions_v0 = artifacts.require("Distributions_v0")
 
-module.exports = function(deployer) {
-  deployer.deploy(SubredditPoints_v0);
-  deployer.deploy(Subscriptions_v0);
-  deployer.deploy(Distributions_v0);
+module.exports = async function(deployer, network, accounts) {
+  const address = accounts[0]
+  
+  await deployer.deploy(SubredditPoints_v0);
+  await deployer.deploy(Distributions_v0);
+
+  const points = await SubredditPoints_v0.deployed()
+
+  const dist = await Distributions_v0.deployed()
+  await points.methods['initialize(address,address,address,string,string,string,address[])'](address,address,address,"Arbitrum", "ArbCommunityPoints", "ARBC",[]);
+  await dist.methods['initialize(address,address,address,address,uint256,uint256,uint256,uint256,uint256,address[],uint256[])'](address, points.address, address, address, '2000000', '1000000', '2000000', '100', '5',[],[])
+  await points.updateDistributionContract(dist.address)
+
+  const symbol = await points.symbol.call()
+  console.log(' *** SubredditPoints_v0: symbol: *** ', symbol);
+
+  const  initSupply = await dist.initialSupply()
+  console.warn('*** Distributions_v0 initial supply ***', initSupply.toString());
+
+
+  
+
+
 };
