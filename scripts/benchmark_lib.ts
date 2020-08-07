e953fb2d667387274c065bafefc52e3f65a40773
dzgoldman <dzgoldman@wesleyan.edu>
2020-08-07 17:01:08 -0400

    add 'tests' verifying updates took place; refactor to calculate L1 gas accross multiple blocks; lint/format

--- scripts/benchmark_lib.ts
+++ scripts/benchmark_lib.ts
@@ -1,121 +1,239 @@
-import { arbWallet, generateSignature, DistributionsContract, PointsContract, SubscriptionsContract, batchMint, getLastRound, l1Provider  } from "./contracts_lib"
-import { ethers, Wallet } from 'ethers'
-import { TransactionResponse, Log } from 'ethers/providers'
-import { BigNumber, formatEther } from 'ethers/utils'
-const karmaConstant = new BigNumber(1000)
-const round = new BigNumber(0)
+import {
+  arbWallet,
+  generateSignature,
+  DistributionsContract,
+  PointsContract,
+  SubscriptionsContract,
+  batchMint,
+  getLastRound,
+  l1Provider,
+  l1Bridge,
+} from "./contracts_lib";
+import { ethers, Wallet } from "ethers";
+import { TransactionResponse, Log } from "ethers/providers";
+import { BigNumber, formatEther } from "ethers/utils";
 
+const chalk = require("chalk");
 
-const printTotalGasUsed = (txnResponses: Promise<TransactionResponse>[],  message: string, next?: () => any)=>{
-    Promise.all(txnResponses).then((responses)=>{
-        
-        const receiptPromises = responses.map((res)=> res.wait());
-        Promise.all(receiptPromises).then((receipts) =>{
-            const totalGasUsed = receipts.reduce((acc, current)=>( acc.add(current.gasUsed)), new BigNumber(0))
-            console.log(message + totalGasUsed)
-            console.log("");
-            
-            next && next()
-        })
-    }).catch((err)=>{
-        console.warn('GAS CALC ERROR', err);
-        
+const karmaConstant = new BigNumber(1000);
+const bigZero = new BigNumber(0);
+const round = bigZero;
+
+const updates = {
+  initialMainBalance: bigZero,
+  claims: {
+    claimedAddresses: [],
+    value: new BigNumber(1000),
+  },
+  transfers: {
+    recipientAddresses: [],
+    value: new BigNumber(1),
+    count: bigZero,
+  },
+  burns: {
+    count: bigZero,
+    value: new BigNumber(1),
+  },
+  subscribes: {
+    value: new BigNumber(1),
+    count: bigZero,
+  },
+};
+
+const printTotalGasUsed = async (
+  txnResponses: Promise<TransactionResponse>[],
+  next?: () => any
+) => {
+  const startBlock = await l1Provider.getBlockNumber();
+
+  Promise.all(txnResponses)
+    .then((responses) => {
+      const receiptPromises = responses.map((res) => res.wait());
+      Promise.all(receiptPromises).then(async (receipts) => {
+        const totalGasUsed = receipts.reduce(
+          (acc, current) => acc.add(current.gasUsed),
+          new BigNumber(0)
+        );
+        console.log(chalk.green(`Used ${totalGasUsed} ArbGas`));
+
+        const endBlock = await l1Provider.getBlockNumber();
+
+        printL1GasUsed(startBlock + 1, endBlock, next);
+      });
     })
-}
+    .catch((err) => {
+      console.warn(chalk.red("GAS CALC ERROR", err));
+    });
+};
 
-export const printL1GasUsed = async (log:Log, message: string) =>{
-    const txnResp = await l1Provider.getTransaction(log.transactionHash)
-    const txnReceipt = await txnResp.wait()
-    console.log(message + txnReceipt.gasUsed);
-    
-}
+const printL1GasUsed = async (
+  startBlockHeight: number,
+  endBlockHeight: number,
+  next?: () => any
+) => {
+  const inbox = await l1Bridge.globalInboxConn();
+  const { MessageDeliveredFromOrigin } = inbox.interface.events;
+  const topics = [MessageDeliveredFromOrigin.topic];
+  const logs = await l1Provider.getLogs({
+    topics,
+    fromBlock: startBlockHeight,
+    toBlock: endBlockHeight,
+  });
+  let totalGasUsed = new BigNumber(0);
+  for (let i = 0; i < logs.length; i++) {
+    const log = logs[i];
+    const txnResp = await l1Provider.getTransaction(log.transactionHash);
+    const txnReceipt = await txnResp.wait();
+    totalGasUsed = totalGasUsed.add(txnReceipt.gasUsed);
+  }
+  console.info(
+    chalk.green(
+      `Used ${totalGasUsed} L1 gas over ${logs.length} ${
+        logs.length === 1 ? "batch" : "batches"
+      }`
+    )
+  );
+  console.info("");
+  next && next();
+};
 
-const randomSignedClaim = async ()=>{
-    const address = Wallet.createRandom().address
-    const signature =   await generateSignature(address,round, karmaConstant)
-    return {
+const randomSignedClaim = async () => {
+  const address = Wallet.createRandom().address;
+  const signature = await generateSignature(address, round, karmaConstant);
+  return {
+    address,
+    signature,
+  };
+};
+
+export const batchClaims = async (count: number, next?: () => any) => {
+  console.info(chalk.blue(`broadcasting ${count} claims...`));
+
+  let txCount = await arbWallet.getTransactionCount();
+  const claims = [];
+  for (let i = 0; i < count; i++) {
+    const signedClaim = await randomSignedClaim();
+    claims.push(
+      DistributionsContract.claim(
+        round,
+        signedClaim.address,
+        karmaConstant,
+        signedClaim.signature,
+        { nonce: txCount }
+      )
+    );
+    txCount++;
+  }
+  printTotalGasUsed(claims, next);
+};
+
+export const batchSubscribes = async (count: number, next?: () => any) => {
+  console.info(chalk.blue(`broadcasting ${count} subscribes...`));
+  updates.subscribes.count = new BigNumber(count);
+  let txCount = await arbWallet.getTransactionCount();
+  const subscribes = [];
+  for (let i = 0; i < count; i++) {
+    subscribes.push(
+      SubscriptionsContract.subscribe(arbWallet.address, false, {
+        nonce: txCount,
+      })
+    );
+    txCount++;
+  }
+  printTotalGasUsed(subscribes, next);
+};
+
+export const setup = async () => {
+  console.info("");
+  console.info(chalk.blue("initializing..."));
+  const { address } = arbWallet;
+  const bal = await PointsContract.balanceOf(address);
+
+  if (bal.isZero()) {
+    console.info(chalk.blue("Transfering tokens to main account..."));
+    const signature = await generateSignature(address, round, karmaConstant);
+    try {
+      const res = await DistributionsContract.claim(
+        round,
         address,
+        karmaConstant,
         signature
+      );
+      await res.wait();
+      const bal = await PointsContract.balanceOf(address);
+      updates.initialMainBalance = bal;
+      console.info(
+        chalk.green(
+          `Successfully claimed ${bal.toNumber()} tokens for main account; good to go!`
+        )
+      );
+    } catch (err) {
+      console.warn(chalk.red("Error claiming tokens for op:", err));
     }
-}
-export const batchClaims  = async (count: number, next?: () => any)=>{
-    console.info(`broadcasting ${count} claims...`)
+  } else {
+    updates.initialMainBalance = bal;
+    console.info(
+      chalk.green(`Main account has ${bal.toNumber()} tokens; good to go!`)
+    );
+  }
+  console.info("");
+};
+export const batchTransfers = async (count: number, next?: () => any) => {
+  console.info(chalk.blue(`broadcasting ${count} transfers...`));
+  updates.transfers.count = new BigNumber(count);
+  const rec = Wallet.createRandom().address;
+  let txCount = await arbWallet.getTransactionCount();
+  const transfers = [];
+  for (let i = 0; i < count; i++) {
+    transfers.push(
+      PointsContract.transfer(rec, updates.transfers.value, { nonce: txCount })
+    );
+    txCount++;
+  }
+  printTotalGasUsed(transfers, next);
+};
 
-    let txCount =  await arbWallet.getTransactionCount()
-    const claims = []
-    for (let i = 0; i < count; i++) {
-        const signedClaim = await randomSignedClaim()
-        claims.push(
-            DistributionsContract.claim(round, signedClaim.address, karmaConstant, signedClaim.signature, {nonce: txCount })
-            )
-        txCount++
-    }
-    printTotalGasUsed(claims, `ArbGas used in ${count} claims: `, next)
-}
+export const batchBurns = async (count: number, next?: () => any) => {
+  updates.burns.count = new BigNumber(count);
+  console.info(chalk.blue(`broadcasting ${count} burns...`));
+  let txCount = await arbWallet.getTransactionCount();
+  const burns = [];
+  for (let i = 0; i < count; i++) {
+    burns.push(
+      PointsContract.burn(updates.burns.value, "0x", { nonce: txCount })
+    );
+    txCount++;
+  }
+  printTotalGasUsed(burns, next);
+};
 
+export const verifyUpdates = async () => {
+  console.info(chalk.blue("Verifying updates:"));
+  const claimBalancePromises = updates.claims.claimedAddresses.map((address) =>
+    PointsContract.balanceOf(address)
+  );
+  Promise.all(claimBalancePromises).then((balances) => {
+    balances.every((balance) => balance.eq(updates.claims.value))
+      ? console.info(chalk.green("Claims successful"))
+      : console.info(chalk.red("Error: claims unsuccessful"));
+  });
 
-export const batchSubscribes = async  (count: number, next?: () => any)=>{
-    console.info(`broadcasting ${count} subscribes...`)
-    let txCount =  await arbWallet.getTransactionCount()
-    const subscribes = []
-    for (let i = 0; i < count; i++) {
-        subscribes.push(
-            SubscriptionsContract.subscribe(arbWallet.address, false, {nonce:txCount})
-            )
-        txCount++
-    }
-    printTotalGasUsed(subscribes, `ArbGas used in ${count} subscribes: `,next)
-}
+  updates.transfers.recipientAddresses.map((address) =>
+    PointsContract.balanceOf(address)
+  );
+  Promise.all(claimBalancePromises).then((balances) => {
+    balances.every((balance) => balance.eq(updates.transfers.value))
+      ? console.info(chalk.green("Transfers successful"))
+      : console.info(chalk.red("Error: Transfers unsuccessful"));
+  });
 
-export const setup = async ()=>{
-    console.info('')
-    console.info('initializing...')
-    const { address } = arbWallet
-    const bal = await PointsContract.balanceOf(address)
-    
-    if (bal.isZero()) {
-        console.warn('Transfering tokens to main account...');
-        const signature =   await generateSignature(address,round, karmaConstant)
-        try { 
-            const res = await DistributionsContract.claim(round, address, karmaConstant, signature)
-            await res.wait()
-            const bal = await PointsContract.balanceOf(address)
-            console.info(`Successfully claimed ${bal.toNumber()} tokens for main account; good to go!`)
-        } catch (err){
-            console.warn('Error claiming tokens for op:', err);
-            
-        }
-    } else {
-        console.info(`Main account has ${bal.toNumber()} tokens; good to go!`)
-    }
-    console.info("")
-
-}
-export const batchTrasfers = async (count: number, next?: () => any)=>{
-    console.info(`broadcasting ${count} transfers...`)
-    const rec = Wallet.createRandom().address
-    let txCount =  await arbWallet.getTransactionCount()
-    const transfers = []
-    for (let i = 0; i < count; i++) {
-        transfers.push(
-            PointsContract.transfer(rec, new BigNumber(1), {nonce:txCount })
-            )
-        txCount++
-    }
-    printTotalGasUsed(transfers, `ArbGas used in ${count} transfers: `, next)
-}
-
-export const batchBurns = async (count: number, next?: () => any )=>{
-    console.info(`broadcasting ${count} burns...`)
-
-    const rec = Wallet.createRandom().address
-    let txCount =  await arbWallet.getTransactionCount()
-    const burns = []
-    for (let i = 0; i < count; i++) {
-        burns.push(
-            PointsContract.burn(1, '0x', {nonce:txCount })
-            )
-        txCount++
-    }
-    printTotalGasUsed(burns, `ArbGas used in ${count} burns: `,next)
-}
+  const { address } = arbWallet;
+  const newBal = await PointsContract.balanceOf(address);
+  const diff = updates.transfers.count
+    .mul(updates.transfers.value)
+    .add(updates.burns.count.mul(updates.burns.value))
+    .add(updates.subscribes.count.mul(updates.subscribes.value));
+  updates.initialMainBalance.sub(diff).eq(newBal)
+    ? console.info(chalk.green("Subscribes and burns successful"))
+    : console.info(chalk.red("Error: Subscribes/burns unsuccessful"));
+};
--- scripts/benchmarks.ts
+++ scripts/benchmarks.ts
@@ -1,53 +1,30 @@
-import { batchClaims, batchSubscribes, batchTrasfers, setup, batchBurns, printL1GasUsed  } from "./benchmark_lib"
+import {
+  batchClaims,
+  batchSubscribes,
+  batchTransfers,
+  setup,
+  batchBurns,
+  verifyUpdates,
+} from "./benchmark_lib";
 
-import {  getLastRound, advanceRound, SubscriptionsContract,l1Bridge, l1Provider, } from "./contracts_lib"
-require('dotenv').config();
+import { l1Provider } from "./contracts_lib";
+require("dotenv").config();
+const chalk = require("chalk");
 
+(async () => {
+  const startBlock = await l1Provider.getBlockNumber();
 
-(async ()=>{
+  await setup();
 
-    const startBlock  = await l1Provider.getBlockNumber()
-    
-    await setup();
+  console.info("");
+  console.info(chalk.blue("*** Running benchmarks ***"));
+  console.info("");
 
-    console.info('')
-    console.info('*** L2 gas benchmarks ***')
-    console.info('')
-
-
-    batchTrasfers(20, ()=>{
-        batchBurns(20, ()=>{
-            batchClaims(20, ()=>{
-                batchSubscribes(20, l1Gas)
-            })
-        })
-    })
-
-    const l1Gas = async ()=> {
-        console.info('')
-        console.info('*** L1 gas benchmarks ***')
-        console.info('')
-        const inbox = await l1Bridge.globalInboxConn()
-        const { MessageDeliveredFromOrigin } = inbox.interface.events
-        const topics = [MessageDeliveredFromOrigin.topic]
-        const logs = await l1Provider
-            .getLogs({
-              topics,
-              fromBlock: startBlock,
-              toBlock: "latest"
-            })
-            if (logs.length === 4){
-                printL1GasUsed(logs[0], "L1 Gas used in transfers batch: ")
-                printL1GasUsed(logs[1], "L1 Gas used in burns batch: ")
-                printL1GasUsed(logs[2], "L1 Gas used in claims batch: ")
-                printL1GasUsed(logs[3], "L1 Gas used in subscribes batch: ")
-                    
-            } else {
-                console.warn(`Error: expected to find 4 messages batches; instead found ${logs.length}. Try again`)
-            }
-            
-    }
-    // l1Gas()
-    
-
-})()
+  batchTransfers(20, () => {
+    batchBurns(20, () => {
+      batchClaims(20, () => {
+        batchSubscribes(20, verifyUpdates);
+      });
+    });
+  });
+})();
