dab8f8fb007a5794c6b591e48786d23698ee66d8
dzgoldman <dzgoldman@wesleyan.edu>
2020-08-13 14:23:08 -0400

    finish claim flow

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -11,135 +11,140 @@ import Claim from "./components/Claim";
 import About from "./components/About";
 import { ClaimStatus } from "./lib/index";
 import {
-  Redirect,
-  Route,
-  Switch,
-  HashRouter,
-  BrowserRouter,
-  useRouteMatch,
-
+    Redirect,
+    Route,
+    Switch,
+    HashRouter,
+    BrowserRouter,
+    useRouteMatch,
 } from "react-router-dom";
 import LandingScreen from "./components/LandingScreen";
 // import { ArbErc20Factory } from 'arb-provider-ethers/dist/lib/abi/ArbErc20Factory'
 
 interface AppProps {
-  ethProvider: ethers.providers.JsonRpcProvider;
+    ethProvider: ethers.providers.JsonRpcProvider;
 }
 function App({ ethProvider }: AppProps) {
-  const { distributionAddress, tokenAddress } = constants;
+    const { distributionAddress, tokenAddress } = constants;
 
-  const [PointsContract, DistributionContract, wallet] = useMemo(() => {
-    if (!ethProvider) return [];
-    const wallet = ethProvider.getSigner(0);
+    const [PointsContract, DistributionContract, wallet] = useMemo(() => {
+        if (!ethProvider) return [];
+        const wallet = ethProvider.getSigner(0);
 
-    return [
-      new Contract(tokenAddress, SubredditPoints_v0, wallet),
-      new Contract(distributionAddress, Distributions_v0, wallet),
-      wallet,
-    ];
-  }, [ethProvider]);
+        return [
+            new Contract(tokenAddress, SubredditPoints_v0, wallet),
+            new Contract(distributionAddress, Distributions_v0, wallet),
+            wallet,
+        ];
+    }, [ethProvider]);
 
-  const [walletAddress, setWalletAddress] = useState("");
-  useEffect(() => {
-    wallet && wallet.getAddress().then(setWalletAddress);
-  }, [wallet]);
+    const [walletAddress, setWalletAddress] = useState("");
+    useEffect(() => {
+        wallet && wallet.getAddress().then(setWalletAddress);
+    }, [wallet]);
 
-  const [currentRound, setCurrentRound] = useState(0);
-  const [userCanClaim, setUserCanClaim] = useState(ClaimStatus.LOADING);
+    const [currentRound, setCurrentRound] = useState(0);
+    const [userCanClaim, setUserCanClaim] = useState(ClaimStatus.LOADING);
 
-  useEffect(() => {
-    if (!DistributionContract || !walletAddress) return;
-    DistributionContract.lastRound().then((round: utils.BigNumber) => {
-      const lastRoundNum = +round.toString();
-      setCurrentRound(lastRoundNum);
-      DistributionContract.claimableRounds(walletAddress).then(
-        (lastClaimedRound: utils.BigNumber) => {
-          console.info("LAST CLAIMED ROUND", lastClaimedRound.toString());
-          console.info("Last Round", lastRoundNum.toString());
+    useEffect(() => {
+        if (!DistributionContract || !walletAddress) return;
+        DistributionContract.lastRound().then((round: utils.BigNumber) => {
+            const lastRoundNum = +round.toString();
+            setCurrentRound(lastRoundNum);
+            DistributionContract.claimableRounds(walletAddress).then(
+                (lastClaimedRound: utils.BigNumber) => {
+                    console.info(
+                        "LAST CLAIMED ROUND",
+                        lastClaimedRound.toString()
+                    );
+                    console.info("Last Round", lastRoundNum.toString());
 
-          setUserCanClaim(
-            round.gte(lastClaimedRound)
-              ? ClaimStatus.CLAIMABLE
-              : ClaimStatus.UNCLAIMABLE
-          );
+                    setUserCanClaim(
+                        round.gte(lastClaimedRound)
+                            ? ClaimStatus.CLAIMABLE
+                            : ClaimStatus.UNCLAIMABLE
+                    );
+                }
+            );
+        });
+    }, [DistributionContract, walletAddress]);
+
+    const [tokenSymbol, setTokenSymbol] = useState("");
+    const [tokenName, setTokenName] = useState("");
+
+    useEffect(() => {
+        if (PointsContract && !tokenSymbol) {
+            PointsContract.symbol().then(setTokenSymbol);
         }
-      );
-    });
-  }, [DistributionContract, walletAddress]);
+        if (DistributionContract && !tokenName) {
+            DistributionContract.initialSupply().then((d: utils.BigNumber) => {
+                setTokenName(d.toString());
+            });
+        }
+    }, [PointsContract, DistributionContract, tokenSymbol]);
 
-  const [tokenSymbol, setTokenSymbol] = useState("");
-  const [tokenName, setTokenName] = useState("");
+    const [tokenBalance, setTokenBalance] = useState(0);
+    const updateTokenBalance = useCallback(() => {
+        if (!PointsContract || !walletAddress) return;
+        PointsContract.balanceOf(walletAddress).then((bal: any) =>
+            setTokenBalance(bal.toNumber())
+        );
+    }, [PointsContract, walletAddress]);
+    // TODO polling update
+    useEffect(updateTokenBalance, [PointsContract, walletAddress]);
 
-  useEffect(() => {
-    if (PointsContract && !tokenSymbol) {
-      PointsContract.symbol().then(setTokenSymbol);
-    }
-    if (DistributionContract && !tokenName) {
-      DistributionContract.initialSupply().then((d: utils.BigNumber) => {
-        setTokenName(d.toString());
-      });
-    }
-  }, [PointsContract, DistributionContract, tokenSymbol]);
-
-  const [tokenBalance, setTokenBalance] = useState(0);
-  const updateTokenBalance = useCallback(() => {
-    if (!PointsContract || !walletAddress) return;
-    PointsContract.balanceOf(walletAddress).then((bal: any) =>
-      setTokenBalance(bal.toNumber())
+    const transferToken = useCallback(
+        async (account: string, value: number) => {
+            if (PointsContract) {
+                const txReceipt = await PointsContract.transfer(
+                    account,
+                    value
+                ).catch((e: any) => alert("Error: " + e.message));
+                return txReceipt;
+            }
+        },
+        [PointsContract]
     );
-  }, [PointsContract, walletAddress]);
-  // TODO polling update
-  useEffect(updateTokenBalance, [PointsContract, walletAddress]);
+    let match = useRouteMatch();
+    const mainRoute = match.path + "/";
+    const claimRoute = match.path + "/claim/:round/:address/:sig";
+    const aboutRoute = match.path + "/about";
 
-  const transferToken = useCallback(
-    async (account: string, value: number) => {
-      if (PointsContract) {
-        const txReceipt = await PointsContract.transfer(
-          account,
-          value
-        ).catch((e: any) => alert("Error: " + e.message));
-        return txReceipt;
-      }
-    },
-    [PointsContract]
-  );
-  let match = useRouteMatch();
-  const mainRoute = match.path + "/"
-  const claimRoute = match.path + "/claim/:round/:address/:sig"
-  const aboutRoute = match.path + "/about"
-
-  return (
-    <Switch>
-      <Route
-        path={mainRoute}
-        render={() => (
-          <Main
-            tokenSymbol={tokenSymbol}
-            tokenName={String(currentRound)}
-            currentRound={currentRound}
-            userCanClaim={userCanClaim}
-            tokenBalance={tokenBalance}
-            setTokenBalance={setTokenBalance}
-            transferToken={transferToken}
-          />
-        )}
-        exact
-      />
-      <Route
-        path={claimRoute}
-        render={(props) => (
-          <Claim
-            currentRound={currentRound}
-            walletAddress={walletAddress}
-            claim={DistributionContract && DistributionContract.claim}
-            {...props}
-          />
-        )}
-        exact
-      />
-      <Route path={aboutRoute} component={About} />
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route
+                path={mainRoute}
+                render={() => (
+                    <Main
+                        tokenSymbol={tokenSymbol}
+                        tokenName={String(currentRound)}
+                        currentRound={currentRound}
+                        userCanClaim={userCanClaim}
+                        tokenBalance={tokenBalance}
+                        setTokenBalance={setTokenBalance}
+                        transferToken={transferToken}
+                    />
+                )}
+                exact
+            />
+            <Route
+                path={claimRoute}
+                render={(props) => (
+                    <Claim
+                        currentRound={currentRound}
+                        walletAddress={walletAddress}
+                        claim={
+                            DistributionContract && DistributionContract.claim
+                        }
+                        userCanClaim={userCanClaim}
+                        {...props}
+                    />
+                )}
+                exact
+            />
+            <Route path={aboutRoute} component={About} />
+        </Switch>
+    );
 }
 
 export default App;
--- ui/src/components/Claim.tsx
+++ ui/src/components/Claim.tsx
@@ -9,192 +9,260 @@ import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Paper from "@material-ui/core/Paper";
 import Alert from "@material-ui/lab/Alert";
-import Header from "./LandingScreen/Header"
+import Header from "./LandingScreen/Header";
 import rollup from "../assets/images/logo.png";
+import { ClaimStatus } from "../lib/index";
+import { Redirect } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => {
-  return {
-    paper: {
-      textAlign: "left",
-      color: theme.palette.text.secondary,
-      minHeight: "8rem",
-    },
-    list: {
-      marginLeft: theme.spacing(2),
-      marginRight: theme.spacing(2),
-      minWidth: "500px",
-    },
-  };
+    return {
+        paper: {
+            textAlign: "left",
+            color: theme.palette.text.secondary,
+            minHeight: "8rem",
+        },
+        list: {
+            marginLeft: theme.spacing(2),
+            marginRight: theme.spacing(2),
+            minWidth: "500px",
+        },
+    };
 });
 
 interface ClaimProps {
-  match: {
-    params: {
-      round: string;
-      address: string;
-      sig: string;
+    match: {
+        params: {
+            round: string;
+            address: string;
+            sig: string;
+        };
     };
-  };
-  walletAddress: string;
-  claim: any;
-  currentRound: number | undefined;
+    walletAddress: string;
+    claim: any;
+    currentRound: number | undefined;
+    userCanClaim: ClaimStatus;
 }
 
 enum ReadyState {
-  Waiting,
-  Valid,
-  InvalidAddress,
-  InvalidRound,
-  InvalidSignature,
+    Waiting,
+    Valid,
+    InvalidAddress,
+    InvalidRound,
+    InvalidSignature,
+    PreviouslyClaimed,
+    JustClaimed,
+    JustSubmitted,
 }
 
-const Claim = ({ match, walletAddress, claim, currentRound }: ClaimProps) => {
-  const { round, address, sig } = match.params;
-  const classes = useStyles();
+enum SubmissionStatus {
+    UNSUBMITTED,
+    SUBMITTED,
+    COMPLETE,
+}
+const Claim = ({
+    match,
+    walletAddress,
+    claim,
+    currentRound,
+    userCanClaim,
+}: ClaimProps) => {
+    const { round, address, sig } = match.params;
+    const classes = useStyles();
+    const [submissionStatus, setSubmissionStatus] = useState<SubmissionStatus>(
+        SubmissionStatus.UNSUBMITTED
+    );
+    const [redirectHome, setRedirectHome] = useState(false);
 
-  const invalidMessage = useCallback(
-    (readyState: ReadyState) => {
-      switch (readyState) {
-        case ReadyState.InvalidAddress:
-          return `Wrong address; make sure you're logged in with $address`;
-        case ReadyState.InvalidRound:
-          return "Looks like this round has already passed!";
-        case ReadyState.InvalidSignature:
-          return "Invalid signaure, try again.";
-        default:
-          throw new Error("Not an invalid readystate");
-      }
-    },
-    [address]
-  );
+    const invalidMessage = useCallback(
+        (readyState: ReadyState) => {
+            switch (readyState) {
+                case ReadyState.InvalidAddress:
+                    return `Wrong address; make sure you're logged in with $address`;
+                case ReadyState.InvalidRound:
+                    return "Looks like this round has already passed!";
+                case ReadyState.InvalidSignature:
+                    return "Invalid signaure, try again.";
+                default:
+                    throw new Error("Not an invalid readystate");
+            }
+        },
+        [address]
+    );
 
-  const readyState: ReadyState = useMemo(() => {
-    if (!walletAddress || !claim || currentRound === undefined) {
-      return ReadyState.Waiting;
-    } else if (walletAddress !== address) {
-      return ReadyState.InvalidAddress;
-    } else if (false /*TODO sig check? */) {
-      return ReadyState.InvalidSignature;
-    } else if (currentRound !== +round) {
-      return ReadyState.InvalidRound;
-      // todo: check if claimable
-    } else {
-      return ReadyState.Valid;
-    }
-  }, [walletAddress]);
+    const readyState: ReadyState = useMemo(() => {
+        if (
+            !walletAddress ||
+            !claim ||
+            currentRound === undefined ||
+            userCanClaim === ClaimStatus.LOADING
+        ) {
+            return ReadyState.Waiting;
+        } else if (walletAddress !== address) {
+            return ReadyState.InvalidAddress;
+        } else if (false /*TODO sig check? */) {
+            return ReadyState.InvalidSignature;
+        } else if (currentRound !== +round) {
+            return ReadyState.InvalidRound;
+        } else if (userCanClaim === ClaimStatus.UNCLAIMABLE) {
+            return ReadyState.PreviouslyClaimed;
+        } else {
+            return ReadyState.Valid;
+        }
+    }, [walletAddress, claim, currentRound, userCanClaim, submissionStatus]);
 
-  const claimCoins = useCallback(() => {
-    if (!claim || readyState !== ReadyState.Valid) return;
-    // TODO: karma constant
-    claim(round, address, 1000, sig);
-  }, [claim, readyState]);
+    const claimCoins = useCallback(async () => {
+        if (!claim || readyState !== ReadyState.Valid) return;
+        try {
+            setSubmissionStatus(SubmissionStatus.SUBMITTED);
+            const tx = await claim(round, address, 1000, sig);
+            await tx.wait();
+            setSubmissionStatus(SubmissionStatus.COMPLETE);
+            window.setTimeout(() => {
+                setRedirectHome(true);
+            }, 2000);
+        } catch (err) {
+            console.warn("err claiming", err);
+        }
+    }, [claim, readyState]);
 
-  const render = (readyState: ReadyState) => {
-    switch (readyState) {
-      case ReadyState.InvalidAddress:
-        return (
-          <Alert severity="error">{`Wrong address; make sure you're logged in with ${address}`}</Alert>
-        );
+    const render = (readyState: ReadyState) => {
+        switch (readyState) {
+            case ReadyState.InvalidAddress:
+                return (
+                    <Alert severity="error">{`Wrong address; make sure you're logged in with ${address}`}</Alert>
+                );
 
-      case ReadyState.InvalidRound:
-        return (
-          <Alert severity="error">
-            Looks like this round has already passed!
-          </Alert>
-        );
+            case ReadyState.InvalidRound:
+                return (
+                    <Alert severity="error">
+                        Looks like this round has already passed!
+                    </Alert>
+                );
 
-      case ReadyState.InvalidSignature:
-        return <Alert severity="error">Invalid signaure</Alert>;
+            case ReadyState.InvalidSignature:
+                return <Alert severity="error">Invalid signaure</Alert>;
+            case ReadyState.PreviouslyClaimed:
+                return (
+                    <Alert severity="info">
+                        You've already claimed this round
+                    </Alert>
+                );
 
-      case ReadyState.Waiting:
-        return (
-          // <Button onClick={claimCoins}>
-            <CircularProgress />
-          // </Button>
-        );
-      case ReadyState.Valid:
-        return (
-          <Button color="primary" variant="contained" onClick={claimCoins}>
-            Claim your points!
-          </Button>
-        );
+            case ReadyState.Waiting:
+                return (
+                    // <Button onClick={claimCoins}>
+                    <CircularProgress />
+                    // </Button>
+                );
+            case ReadyState.Valid:
+                switch (submissionStatus) {
+                    case SubmissionStatus.UNSUBMITTED:
+                        return (
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                onClick={claimCoins}
+                            >
+                                Claim your points!
+                            </Button>
+                        );
+                    case SubmissionStatus.SUBMITTED:
+                        return <CircularProgress />;
+                    case SubmissionStatus.COMPLETE: {
+                        return (
+                            <Alert severity="success">
+                                Coins Claimed! Redirecting home...
+                            </Alert>
+                        );
+                    }
+                    default:
+                        break;
+                }
 
-      default:
-        break;
-    }
-  };
+            default:
+                break;
+        }
+    };
 
-  const img = <img draggable={false} src={rollup} alt="Arbitrum Rollup" style={{ height: "50px" }}></img>;
+    const img = (
+        <img
+            draggable={false}
+            src={rollup}
+            alt="Arbitrum Rollup"
+            style={{ height: "50px" }}
+        ></img>
+    );
 
-  return (
-    <>
-    <Header title="Arbitrum Comunity Points" img={img} />
-    <Grid
-      container
-      spacing={4}
-      direction="column"
-      alignItems="center"
-      style={{
-        margin: 0,
-        width: '100%',
-      }}
-    >
-      <Grid item />
-      <Grid item component={Paper} className={classes.paper}>
-          <List className={classes.list}>
-            <ListItem>
-              <TextField
-                label="Round Number"
-                placeholder="Placeholder"
-                fullWidth
-                margin="normal"
-                disabled
-                value={round}
-                InputLabelProps={{
-                  shrink: true,
+    return (
+        <>
+            <Header title="Arbitrum Comunity Points" img={img} />
+            <Grid
+                container
+                spacing={4}
+                direction="column"
+                alignItems="center"
+                style={{
+                    margin: 0,
+                    width: "100%",
                 }}
-                InputProps={{
-                  disableUnderline: true,
-                }}
-              />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <TextField
-                label="Address"
-                fullWidth
-                margin="normal"
-                disabled
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                InputProps={{
-                  disableUnderline: true,
-                }}
-                value={address}
-              />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <TextField
-                label="Signature"
-                fullWidth
-                disabled
-                value={sig}
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                InputProps={{
-                  disableUnderline: true,
-                }}
-              />
-            </ListItem>
-          </List>
-      </Grid>
-      <Grid item>{render(readyState)}</Grid>
-    </Grid>
-    </>
-  );
+            >
+                <Grid item />
+                <Grid item component={Paper} className={classes.paper}>
+                    <List className={classes.list}>
+                        <ListItem>
+                            <TextField
+                                label="Round Number"
+                                placeholder="Placeholder"
+                                fullWidth
+                                margin="normal"
+                                disabled
+                                value={round}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                InputProps={{
+                                    disableUnderline: true,
+                                }}
+                            />
+                        </ListItem>
+                        <Divider />
+                        <ListItem>
+                            <TextField
+                                label="Address"
+                                fullWidth
+                                margin="normal"
+                                disabled
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                InputProps={{
+                                    disableUnderline: true,
+                                }}
+                                value={address}
+                            />
+                        </ListItem>
+                        <Divider />
+                        <ListItem>
+                            <TextField
+                                label="Signature"
+                                fullWidth
+                                disabled
+                                value={sig}
+                                margin="normal"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                InputProps={{
+                                    disableUnderline: true,
+                                }}
+                            />
+                        </ListItem>
+                    </List>
+                </Grid>
+                {redirectHome && <Redirect to="/ui" />}
+                <Grid item>{render(readyState)}</Grid>
+            </Grid>
+        </>
+    );
 };
 export default Claim;
