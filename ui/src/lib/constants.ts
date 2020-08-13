562fecbd73741c0eeff6a95242c952d81f3fe469
dzgoldman <dzgoldman@wesleyan.edu>
2020-08-13 11:06:32 -0400

    auto-gen .env file for frontent, reconfig tsconfig and pre-commit

--- ui/src/lib/constants.ts
+++ ui/src/lib/constants.ts
@@ -1,7 +1,6 @@
 export default {
-  localValidatorUrl: "http://127.0.0.1:8547",
-  distributionAddress: "0xD3d21714F1A7F1575B827C15528189EFF61B7F20",
-  tokenAddress: "0x6a1d27B150e2c4E117655B752ea484aCf062B807",
-  infuraId: process.env.INFURA_PROJECT_ID,
-  infuraSecret: process.env.INFURA_PROJECT_SECRET,
+  localValidatorUrl: process.env.REACT_APP_LOCAL_VALIDATOR_URL,
+  distributionAddress: process.env.REACT_APP_DISTRIBUTION_CONTRACT_ADDRESS,
+  tokenAddress: process.env.REACT_APP_POINTS_CONTRACT_ADDRESS
+
 };
